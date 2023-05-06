bbe7837492a2312cfc7367399c34458529b29d07
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-05-06 11:55:49 +0200

    .env var

--- src/config/Page.jsx
+++ src/config/Page.jsx
@@ -5,12 +5,12 @@ console.log(userData);
 
 export default async function handler(request, response) {
   // const { Username, Email, Password, ConfirmPassword, Club, District, Position, Skill, Experience, Social, Competition, League } = request;
-  const connectionString = "postgresql://SeanDylan1982:L2Uo6JhzZHbD@ep-patient-river-074563.eu-central-1.aws.neon.tech/neondb";
-  const client = await db.connect(connectionString);
+  // const connectionString = "postgresql://SeanDylan1982:L2Uo6JhzZHbD@ep-patient-river-074563.eu-central-1.aws.neon.tech/neondb";
+  const client = await db.connect(".env.production.POSTGRES_URL");
   try {
     await client.sql`CREATE TABLE Users ( Username varchar(255), Email varchar(255), Password varchar(255), ConfirmPassword varchar(255), Club varchar(255), District varchar(255), Position varchar(255), Skill varchar(255), Experience varchar(255), Social varchar(255), Competition varchar(255), League varchar(255) );`;
 
-    const { Username, Email, Password, ConfirmPassword, Club, District, Position, Skill, Experience, Social, Competition, League } = request.userData;
+    const { Username, Email, Password, ConfirmPassword, Club, District, Position, Skill, Experience, Social, Competition, League } = request;
 
     await client.sql`INSERT INTO Users (Username, Email, Password, ConfirmPassword, Club, District, Position, Skill, Experience, Social, Competition, League) VALUES (${Username}, ${Email}, ${Password}, ${ConfirmPassword}, ${Club}, ${District}, ${Position}, ${Skill}, ${Experience}, ${Social}, ${Competition}, ${League} });`;
 
@@ -20,6 +20,8 @@ export default async function handler(request, response) {
   const users = await client.sql`SELECT * FROM Users;`;
   return response.status(200).json({ users });
 }
+
+
 
 // export default async function users({ params }) {
 //   const { rows } = await sql`SELECT * from users where user_id=${params.user}`;
