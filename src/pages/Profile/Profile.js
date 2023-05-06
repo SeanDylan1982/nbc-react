90c5488f8214b39dc716b89be2b425d134cba80e
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-05-06 11:05:13 +0200

    env and profile userData store write to db

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,6 +2,12 @@ import Page from 'material-ui-shell/lib/containers/Page'
 import React from 'react'
 import LayoutProfile from '../../components/Layout/LayoutProfile'
 // import AppBar from '../../components/AppBar/AppBar'
+import handler from '../../config/Page'
+
+const userData = localStorage.getItem("auth");
+console.log(userData);
+
+handler();
 
 const ProfilePage = () => {
 
